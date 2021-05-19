e72edb6be78b809599f79e182cda1e61cae04024
Brandon Mercado <Brandon.Mercado@ibm.com>
2021-05-19 12:59:56 -0400

    Added text fields and button

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Lead Time</h1>
+      <h3>From code pushed to code deployed: 9 minutes</h3>
+      <button id="leadTimeSubmit">Submit</button>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("Test Lead Time display", () => {
+    render(<App />);
+    const heading = screen.getByRole("heading", { level: 1});
+
+    expect(heading).toHaveTextContent("Lead Time")
+  });
+
+  test("Code commit to Deploy", () => {
+    render(<App />);
+    const heading = screen.getByRole("heading", { level: 3});
+
+    expect(heading).toHaveTextContent("From code pushed to code deployed: 9 minutes")
+  });
+
+  test("Submit button displays on screen", () => {
+    render(<App />);
+    const addLeadTimeButton = screen.getByRole("button", {id: "leadTimeSubmit"});
+
+    expect(addLeadTimeButton).toBeInTheDocument();
+  });
 });
