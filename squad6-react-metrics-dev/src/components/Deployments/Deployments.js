549a10bf193692ec85443db2f143b39d6fd8be43
Omar Azzam <omar.azzam@ibm.com>
2021-05-19 16:00:15 -0400

    Add test cases for adding deployments

--- squad6-react-metrics-dev/src/components/Deployments/Deployments.js
+++ squad6-react-metrics-dev/src/components/Deployments/Deployments.js
@@ -3,22 +3,53 @@ import '../../global.css';
 
 function Deployments() {
     const [deployment, setDeployment] = useState("");
-    const [newInput, setNewInput] = useState();
+    const [newInputDate, setNewInputDate] = useState();
+    const [newInputTime, setNewInputTime] = useState();
+    const [deployments, setDeployments] = useState([])
 
-    function inputHandler({target: {value}}) {
-        setNewInput(value);
+    function inputHandlerDate({target: {value}}) {
+        //const listDeployment = [];
+
+        setNewInputDate(value);
+      }
+
+      function inputHandlerTime({target: {value}}) {
+        //const listDeployment = [];
+
+        setNewInputTime(value);
       }
     
       function deploymentHandler(){
-        setDeployment(newInput);
+      
+        setDeployments(deployments.concat(newInputDate + " " + newInputTime))
+      
       }
+      const deploymentItems = deployments.map((item) => (
+       
+            <li role='deployment' key={item}>{item}</li>
+        
+      ))
     return(
         <div className='box-container'>
             <h2>Deployments</h2>
-            <div role='deploymentList'>{deployment}</div>
+            <div role='deploymentList'><ol type="1">{deploymentItems}</ol></div>
             <h3>Deployment</h3>
-            <input role="deploymenttime" onChange={inputHandler} className='deploymentDate'/>  
-            <button key='addDeployment' onClick={deploymentHandler} className='button'>Add Deployment</button>
+            <input 
+                role="deploymentdate" 
+                onChange={inputHandlerDate} 
+                className='deploymentDate'
+                type='date'
+            /> 
+        
+            <input 
+                role="deploymenttime" 
+                onChange={inputHandlerTime} 
+                type='time'
+            />
+            <button 
+                key='addDeployment' 
+                onClick={deploymentHandler} 
+                className='button'>Add Deployment</button>
         </div>
     )
 }
--- squad6-react-metrics-dev/src/components/Deployments/Deployments.test.js
+++ squad6-react-metrics-dev/src/components/Deployments/Deployments.test.js
@@ -17,45 +17,89 @@ describe('Deployments', () => {
   
     test("renders  Datetime", () =>{
       render(<Deployments />);
-      //const datetime = screen.getByAltText(/deploymenttime/i)
-      const datetime = screen.getByRole('deploymenttime');
-      expect(datetime).toHaveTextContent("")
+      const date = screen.getByRole('deploymentdate');
+      expect(date).toHaveTextContent("")
     })
     test("Renders Datetime with value after adding value", () => {
       render(<Deployments />);
-      //const tempValue = "2021-05-06";
-     
-      //screen.getByRole('deploymenttime'). = tempValue
-      const datetime = screen.getByRole('deploymenttime');
-      //datetime.textContent = '2021-05-06';
-      userEvent.type(datetime,'2021-05-06');
-  
-      expect(datetime).toHaveValue('2021-05-06');
-      //userEvent.click(screen.getByRole("button", { name: "Add Deployment" }));
-  
-      //expect(status).toHaveTextContent("1");
-  
+      const tempValue = '2021-06-02';
+      let date = screen.getByRole('deploymentdate');
+      userEvent.type(date,tempValue);
+
+      expect(date).toHaveValue(tempValue);
+        
     });
     
     test("Render submit button", () => {
       render(<Deployments />);
-     
-      
       const addButton = screen.getByRole("button", { name: "Add Deployment" });
       expect(addButton).toBeInTheDocument();
   
     });
   
-    test("Add deployment test", () => {
+    test("Add deployment date", () => {
       render(<Deployments />);
-     
-      const datetime = screen.getByRole('deploymenttime');
-      userEvent.type(datetime,'2021-05-06');
+      const tempValue = '2021-06-02';
+      const date = screen.getByRole('deploymentdate');
+      userEvent.type(date,tempValue);
+      
   
       userEvent.click(screen.getByRole("button", { name: "Add Deployment" }));
-      const deployment = screen.getByRole("deploymentList");
-      expect(deployment).toHaveTextContent('2021-05-06');
+      const deployment = screen.getAllByRole("deployment");
+      expect(deployment.length).toEqual(1);
+      expect(deployment[0]).toHaveTextContent(tempValue);
   
     });
+
+    test("Add deployment date and time", () => {
+        render(<Deployments />);
+        const tempDate = '2021-06-02';
+        const date = screen.getByRole('deploymentdate');
+        userEvent.type(date,tempDate);
+        
+        const tempTime = "03:26"
+        const time = screen.getByRole('deploymenttime');
+        userEvent.type(time,tempTime);
+        
+    
+        userEvent.click(screen.getByRole("button", { name: "Add Deployment" }));
+        const deployment = screen.getAllByRole("deployment");
+        expect(deployment.length).toEqual(1);
+        expect(deployment[0]).toHaveTextContent(tempDate + " " + tempTime);
+    
+      });
+
+      test("Add multiple deployment date and time", () => {
+        render(<Deployments />);
+        const tempDate1 = '2021-06-02';
+        const date1 = screen.getByRole('deploymentdate');
+        userEvent.type(date1,tempDate1);
+        
+        const tempTime1 = "03:26"
+        const time1 = screen.getByRole('deploymenttime');
+        userEvent.type(time1,tempTime1);
+        
+    
+        userEvent.click(screen.getByRole("button", { name: "Add Deployment" }));
+        let deployment = screen.getAllByRole("deployment");
+        expect(deployment.length).toEqual(1);
+        expect(deployment[0]).toHaveTextContent(tempDate1 + " " + tempTime1);
+
+        const tempDate2 = '2021-06-03';
+        const date2 = screen.getByRole('deploymentdate');
+        userEvent.type(date2,tempDate2);
+        
+        const tempTime2 = "05:26"
+        const time2 = screen.getByRole('deploymenttime');
+        userEvent.type(time2,tempTime2);
+        
+    
+        userEvent.click(screen.getByRole("button", { name: "Add Deployment" }));
+        deployment = screen.getAllByRole("deployment");
+        expect(deployment.length).toEqual(2);
+        expect(deployment[1]).toHaveTextContent(tempDate2 + " " + tempTime2);
+
+    
+      });
 
 })
