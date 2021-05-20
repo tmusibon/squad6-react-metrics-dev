68dacbacd4ce504984ab69d9598d504959d89940
Thierry Musibon <thierrymusibon@Thierrys-MacBook-Pro.local>
2021-05-19 21:40:09 -0400

    adding style to the button

--- src/components/Deployments/Deployments.js
+++ src/components/Deployments/Deployments.js
@@ -2,19 +2,33 @@ import React,{useState} from "react";
 import '../../global.css';
 
 function Deployments() {
-    const [deployment, setDeployment] = useState("");
-    const [newInputDate, setNewInputDate] = useState();
-    const [newInputTime, setNewInputTime] = useState();
+    const [deployment, setDeployment] = useState('');
+    const [newInputDate, setNewInputDate] = useState('');
+    const [newInputTime, setNewInputTime] = useState('');
     const [deployments, setDeployments] = useState([])
+    //disable the add Deployment button when an input is empty
+    const enabled = newInputDate.length > 0 && 
+                    newInputTime.length > 0
+    
+    const styleEnabled = {
+        backgroundColor: '#008CBA',/* blue */
+        border: '2px solid black',
+        color: 'white',
+        padding: '12px',
+        textAlign: 'center',
+        display: 'block',
+        fontSize: '16px',
+        marginTop: '15px',
+        cursor: 'pointer',
+        marginBottom: '15px'   
+    } 
 
     function inputHandlerDate({target: {value}}) {
-        //const listDeployment = [];
 
         setNewInputDate(value);
       }
 
       function inputHandlerTime({target: {value}}) {
-        //const listDeployment = [];
 
         setNewInputTime(value);
       }
@@ -49,7 +63,15 @@ function Deployments() {
             <button 
                 key='addDeployment' 
                 onClick={deploymentHandler} 
-                className='button'>Add Deployment</button>
+                className='button'
+                disabled={!enabled}
+                style={newInputDate.length > 0 && 
+                        newInputTime.length > 0 ? 
+                        {styleEnabled}: {backgroundColor: '#c3bbbb', 
+                        color:'black', cursor: 'default'}}
+            >
+                Add Deployment
+            </button>
         </div>
     )
 }
